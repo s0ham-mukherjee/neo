2c53bee01b14bab919ada78c5a7412f8f189f569
Soham Mukherjee <sohammukherjee539@gmail.com>
2026-08-16 12:31:13 +0530

    Add files via upload

---
+++ include/optimizer.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "ir.hpp"
+
+namespace ir {
+
+class Optimizer {
+public:
+    explicit Optimizer(int level = 1) : level_(level) {}
+
+    void optimize(IRProgram& program);
+
+private:
+    int level_;
+
+    void optimizeFunction(IRFunction& fn);
+    bool foldConstants(IRFunction& fn);
+    bool algebraicSimplification(IRFunction& fn);
+    bool eliminateDeadCode(IRFunction& fn);
+    bool commonSubexpressionElimination(IRFunction& fn);
+};
+
+} // namespace ir
